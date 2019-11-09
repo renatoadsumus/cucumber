0fdf508dddf76b06f97f9c34315e48eb8ddf0d27
unknown <renatoadsumus@gmail.com>
2019-11-09 09:49:36 -0300

    Criando repo

---
+++ src/test/java/StepDefinitions.java
@@ -0,0 +1,32 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.pt.*;
+
+public class StepDefinitions {
+	
+	@Dado("^que eu tenha em estoque um produto que custe (\\d+) reais$")
+	public void que_eu_tenha_em_estoque_um_produto_que_custe_reais(int arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	  
+	}
+
+	@Quando("^selecionar o produto na barra de pesquisa$")
+	public void selecionar_o_produto_na_barra_de_pesquisa() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Quando("^realizar o pagamento$")
+	public void realizar_o_pagamento() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Então("^não recebo nenhum desconto$")
+	public void não_recebo_nenhum_desconto() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+}
+
+
