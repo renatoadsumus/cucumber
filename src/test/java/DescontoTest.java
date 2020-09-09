3d7155bc73f3cef5a450c3d14ed7a74b18f30275
EC2 Default User <ec2-user@ip-172-31-48-34.ec2.internal>
2020-09-09 14:33:56 +0000

    Teste

--- src/test/java/DescontoTest.java
+++ src/test/java/DescontoTest.java
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> 0312bc00d6d9fb019842f1ceeaf764640121e727
 import static org.junit.Assert.*;
 
 import org.junit.*;
 
-<<<<<<< HEAD
+
 public class DescontoTest{
     
     
@@ -23,44 +19,3 @@ public class DescontoTest{
     
     
 }
-
-
-
-
-
-
-
-=======
-
-public class DescontoTest {
-
-    private Desconto desconto;
-
-    @Before
-	public void antesDeCadaTeste(){
-	    
-		desconto = new Desconto();
-	}
-        
-    @Test
-	public void compraDe10ReaisSemDesconto()
-	{
-		
-		assertTrue(true);
-	}
-	
-	@Test
-	public void compraDe100Reais10PorcentoDesconto()
-	{
-		
-		assertTrue(true);
-	}
-	
-	@Test
-	public void compraDe200Reais20PorcentoDesconto()
-	{
-		
-		assertTrue(true);
-	}
-}
->>>>>>> 0312bc00d6d9fb019842f1ceeaf764640121e727
