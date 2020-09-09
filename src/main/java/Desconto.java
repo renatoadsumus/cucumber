6ff0b4400f4b7f4d931fc084fc28366d7b7f6b1b
EC2 Default User <ec2-user@ip-172-31-48-34.ec2.internal>
2020-09-09 22:18:30 +0000

    Novos Testes Incluidos

--- src/main/java/Desconto.java
+++ src/main/java/Desconto.java
@@ -2,6 +2,16 @@ public class Desconto {
 
 public int CalcularDesconto(int valor){
       
+       if( valor>= 300)
+        return 25;
+        
+      if( valor> 200)
+        return 20;
+        
+    if( valor> 100)
+        return 10;
+      
+        
         
         return 0;
     }
--- src/test/java/DescontoTest.java
+++ src/test/java/DescontoTest.java
@@ -5,17 +5,72 @@ import org.junit.*;
 
 public class DescontoTest{
     
-    
+ Desconto desconto;
+ 
+ @Before
+	public void antesDeCadaTeste(){
+	    
+		  desconto = new Desconto();
+	}
+	
  @Test
  public void SemDesconto () {
-     
-     Desconto desconto = new Desconto();
+    
      int resultadoAtual = desconto.CalcularDesconto(10);
      int resultadoEsperado = 0; 
     
      assertEquals(resultadoEsperado, resultadoAtual);
      
  }
+   
+ @Test
+ public void DescontoDez()
+ {
+    int resultadoAtual = desconto.CalcularDesconto(150);
+    int resultadoEsperado = 10; 
     
+     assertEquals(resultadoEsperado, resultadoAtual);
+     
+  
+ }
+ 
+ @Test
+ public void DescontoVinte()
+ {
+  
+    int resultadoAtual = desconto.CalcularDesconto(250);
+    int resultadoEsperado = 20; 
     
+     assertEquals(resultadoEsperado, resultadoAtual);
+     
+  
+ }
+ @Test
+ public void DescontoTrinta()
+ {
+   Desconto desconto = new Desconto();
+  
+    int resultadoAtual = desconto.CalcularDesconto(350);
+    int resultadoEsperado = 25; 
+    
+     assertEquals(resultadoEsperado, resultadoAtual);
+     
+  
+ }
+ 
+ @Test
+ public void CompraDe300ReaisDescontoDeVintePorcento()
+ {
+   Desconto desconto = new Desconto();
+  
+    int resultadoAtual = desconto.CalcularDesconto(300);
+    int resultadoEsperado = 25; 
+    
+     assertEquals(resultadoEsperado, resultadoAtual);
+     
+  
+ }
+ 
+ 
+ 
 }
